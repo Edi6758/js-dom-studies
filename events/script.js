5a599bf4eff2bf8c421bf3efa7277719c15a0bad
Edi6758 <ediarts7@gmail.com>
2022-12-01 18:45:49 -0300

    :rotating_light: test user github
    test user github

--- events/script.js
+++ events/script.js
@@ -1,5 +1,6 @@
 //event is a dynamic way of executing JS inside HTML
 //the events are triggered according to some specific action on the page, such as clicking, hovering, focusing, etc.
+//test user github...
 
 function mouseEnterEvent() {
     console.log('mouse entered')

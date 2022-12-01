06fe53edbb55dd5ff395ff97f3863417a488d329
edivaldo.junior <edivaldo.junior@acct.global>
2022-12-01 18:41:52 -0300

    :tada: first commit and event study in javascript
    first commit and event study in javascript

---
+++ events/script.js
@@ -0,0 +1,22 @@
+//event is a dynamic way of executing JS inside HTML
+//the events are triggered according to some specific action on the page, such as clicking, hovering, focusing, etc.
+
+function mouseEnterEvent() {
+    console.log('mouse entered')
+}
+
+function changeEvent() {
+    console.log('value changed')
+}
+
+function focusEvent() {
+    console.log('gained focus')
+}
+
+function blurEvent() {
+    console.log('lost focus')
+}
+
+function clickEvent() {
+    alert('clicked')
+}
